9a3b9b373c5ba0285e4ddf21310a15591591713e
Eleni Chappen <eleni@20spokes.com>
2018-07-08 10:04:30 -0500

    Notes on user authentication in preparation for Tuesday's pairing session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    
+
   end
 
   def new
@@ -8,13 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    # EC: Let's talk Tuesday about how to whitelist parameters in controllers.
     user = User.create(
                         first_name: params[:first_name],
                         last_name: params[:last_name],
                         password: params[:password],
                         password_confirmation: params[:password_confirmation])
-
-    redirect_to "/userinfo/#{user.id}"            
+    # EC: If you'd like to log in this user when they register, set the session's user id here as well,
+    # or else current_user will be nil.
+    # Alternatively, if you don't want to log them in immediately, you could redirect them to the
+    # login page instead of the user detail page.
+    redirect_to "/userinfo/#{user.id}"
   end
 
   def show
@@ -32,15 +36,13 @@ class UsersController < ApplicationController
                  password: params[:password],
                  password_confirmation: params[:password_confirmation])
 
-    redirect_to "/userinfo/#{@user.id}"            
+    redirect_to "/userinfo/#{@user.id}"
   end
 
   def destroy
     @user = User.find(params[:id])
   end
-  
 
-  
   # def clock_in
   #   @user = User.find(params[:id])
   # end
